5e7ea225085e753404fc9a48bd1d887b1fb67a6b
Frey Khademi <frey.khademi@gmail.com>
2020-10-23 13:58:29 +0200

    update

--- aws-copilot/var.tf
+++ aws-copilot/var.tf
@@ -5,5 +5,4 @@ variable "subnet_id" {}
 variable "vpc_id" {}
 variable "region" { default = "eu-central-1" }
 variable "instance_size" { default = "t2.medium" }
-variable "keyname" { default = "avx-copilot-ssh-key" }
 variable "instance_name" { default = "aviatrix-copilot" }
